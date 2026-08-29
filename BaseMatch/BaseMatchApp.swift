07b12790cb89c379d7dcb1c46a9ff83643ca428b
y_as_u_16 <egys16@egi-engineer.com>
2026-08-29 11:13:43 +0900

    Initial Commit

---
+++ BaseMatch/BaseMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseMatchApp.swift
+//  BaseMatch
+//
+//  Created by えぎやすゆき on 2026/08/29.
+//
+
+import SwiftUI
+
+@main
+struct BaseMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaseMatchTests/BaseMatchTests.swift
@@ -0,0 +1,18 @@
+//
+//  BaseMatchTests.swift
+//  BaseMatchTests
+//
+//  Created by えぎやすゆき on 2026/08/29.
+//
+
+import Testing
+
+struct BaseMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
